c827136532dbf8ed3618566d935f4929f757af24
shellclear <12013211+shellclear@users.noreply.github.com>
2022-04-07 19:39:22 -0400

    add .vimrc yaml config

--- bootstrap-control-node-automation-user-actions.sh
+++ bootstrap-control-node-automation-user-actions.sh
@@ -3,3 +3,6 @@
 echo "configure automation user ssh key"
 ansible localhost -uautomation -m openssh_keypair -a "path=files/automation_ssh_rsa"
 
+echo "configure .vimrc file"
+ansible localhost -uautomation -m copy -a 'content="autocmd FileType yaml setlocal ai ts=2 sw=2 et" dest="${HOME}/.vimrc"'
+
